12746393d1e383bfa5d18f5a614aa149a27f7760
James Addison <james@reciperadar.com>
2020-01-20 08:00:19 -0800

    Add TODO

--- reciperadar/models/recipes/recipe.py
+++ reciperadar/models/recipes/recipe.py
@@ -69,7 +69,7 @@ class Recipe(Storable, Searchable):
             id=recipe_id,
             title=doc['title'],
             src=doc['src'],
-            dst=doc['dst'],
+            dst=doc.get('dst'),  # TODO: Backwards compatibility; update
             domain=doc['domain'],
             image_src=doc.get('image_src'),
             ingredients=[
