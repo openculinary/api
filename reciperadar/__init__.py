129c7344b317365b4178d2f334d6a386b6dcb101
James Addison <james@reciperadar.com>
2020-05-27 20:06:52 +0100

    Diverge from Backend: stand down API queue workers (#67)

--- reciperadar/__init__.py
+++ reciperadar/__init__.py
@@ -5,8 +5,6 @@ from flask_cors import CORS
 from flask_mail import Mail
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-from reciperadar.services.database import DB_URI
-
 
 app = Flask(__name__)
 app.config.update(
@@ -15,8 +13,6 @@ app.config.update(
     MAIL_USE_TLS=True,
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD'),
-    SQLALCHEMY_DATABASE_URI=DB_URI,
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
 app.url_map.strict_slashes = False
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
@@ -29,7 +25,6 @@ mail = Mail(app)
 
 
 import reciperadar.api.feedback
-import reciperadar.api.products
 import reciperadar.api.recipes
 import reciperadar.api.redirect
 import reciperadar.api.search
--- reciperadar/workers/searches.py
+++ reciperadar/workers/searches.py
@@ -1,25 +1,6 @@
-import requests
-
 from reciperadar.workers.broker import celery
 
 
 @celery.task(queue='recrawl_search')
 def recrawl_search(include, exclude, equipment, offset):
-    params = {
-        'include[]': include,
-        'exclude[]': exclude,
-        'equipment[]': equipment,
-        'offset': offset
-    }
-    response = requests.post(
-        url='http://recrawler-service',
-        params=params
-    )
-
-    try:
-        response.raise_for_status()
-    except Exception as e:
-        print(f'Exception during recrawler request: {e}')
-        return []
-
-    return response.json()
+    pass
