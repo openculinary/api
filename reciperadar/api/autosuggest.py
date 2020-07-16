5f0ad7cd0dccb2267918202c40247ace58b1cc81
James Addison <james@reciperadar.com>
2020-07-16 15:55:25 +0100

    Add common /autosuggest path prefix

--- reciperadar/api/autosuggest.py
+++ reciperadar/api/autosuggest.py
@@ -5,14 +5,14 @@ from reciperadar.models.recipes import RecipeEquipment
 from reciperadar.models.recipes import RecipeIngredient
 
 
-@app.route('/equipment')
+@app.route('/autosuggest/equipment')
 def equipment():
     prefix = request.args.get('pre')
     results = RecipeEquipment().autosuggest(prefix)
     return jsonify(results)
 
 
-@app.route('/ingredients')
+@app.route('/autosuggest/ingredients')
 def ingredients():
     prefix = request.args.get('pre')
     results = RecipeIngredient().autosuggest(prefix)
