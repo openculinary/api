106a16719d2dfae992be328dc93900525c8934c7
James Addison <james@reciperadar.com>
2020-11-18 20:25:18 +0000

    Remove the IngredientProduct database model (#89)

--- reciperadar/models/recipes/product.py
+++ reciperadar/models/recipes/product.py
@@ -4,14 +4,13 @@ from reciperadar import db
 from reciperadar.models.base import Storable
 
 
-class IngredientProduct(Storable):
+class Product(Storable):
     __tablename__ = 'ingredient_products'
 
     fk = db.ForeignKey('recipe_ingredients.id', ondelete='cascade')
     ingredient_id = db.Column(db.String, fk, index=True)
 
     id = db.Column(db.String, primary_key=True)
-    product_id = db.Column(db.String)
     product = db.Column(db.String)
     product_parser = db.Column(db.String)
     is_plural = db.Column(db.Boolean)
@@ -25,14 +24,12 @@ class IngredientProduct(Storable):
 
     @staticmethod
     def from_doc(doc):
-        product_id = doc.get('id') or IngredientProduct.generate_id()
         plural = doc.get('plural')
         singular = doc.get('singular')
         is_plural = doc.get('is_plural')
         product = plural if is_plural else singular
-        return IngredientProduct(
-            id=product_id,
-            product_id=doc.get('product_id'),
+        return Product(
+            id=doc.get('id'),
             product=product,
             product_parser=doc.get('product_parser'),
             is_plural=is_plural,
@@ -44,15 +41,15 @@ class IngredientProduct(Storable):
 
     def state(self, include):
         states = {
-            True: IngredientProduct.STATE_AVAILABLE,
-            False: IngredientProduct.STATE_REQUIRED,
+            True: Product.STATE_AVAILABLE,
+            False: Product.STATE_REQUIRED,
         }
         available = bool(set(self.contents or []) & set(include or []))
         return states[available]
 
     def to_dict(self, include):
         return {
-            'product_id': self.product_id,
+            'product_id': self.id,
             'product': self.product,
             'category': self.category,
             'singular': self.singular,
