d6f517d286fa5e021fafc29deb97f9b982a493c2
James Addison <james@reciperadar.com>
2021-05-22 19:29:46 +0100

    Improvement: Kubernetes credential creation script: wrap credential values as quoted strings

--- k8s/scripts/create-secrets.sh
+++ k8s/scripts/create-secrets.sh
@@ -6,5 +6,5 @@ read -s password
 
 kubectl delete secret api-contact-mail
 kubectl create secret generic api-contact-mail \
-  --from-literal=username=${username} \
-  --from-literal=password=${password}
+  --from-literal=username="${username}" \
+  --from-literal=password="${password}"
