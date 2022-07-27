31d25b290f783828d8e90d2b48c3bb1637ace532
James Addison <james@reciperadar.com>
2022-07-27 19:54:38 +0100

    Fixup: variable shadowing introduced by 46ad3f11a159763770d4688eb4b2e202feb59a0d

--- reciperadar/models/feedback.py
+++ reciperadar/models/feedback.py
@@ -10,7 +10,7 @@ class Feedback(object):
 
         with app.app_context():
             title = issue.pop("issue") or "(empty)"
-            title = issue if len(issue) < 25 else f"{issue[:25]}..."
+            title = title if len(title) < 25 else f"{title[:25]}..."
 
             html = "<html><body><table>"
             for k, v in issue.items():
@@ -18,7 +18,7 @@ class Feedback(object):
                 html += f"<th>{k}</th>"
                 html += f"<td>{v}</td>"
                 html += "</tr>"
-            html += f"</table><hr /><p>{issue}</p></body></html>"
+            html += f"</table><hr /><p>{title}</p></body></html>"
 
             message = Message(
                 subject=f"User feedback: {title}",
