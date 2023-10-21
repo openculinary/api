0fbc90a896f3162c1d2826b6ff31fb567e53d789
James Addison <james@reciperadar.com>
2023-10-21 15:40:00 +0100

    search: refinement tweaks:
      * only relax the number of required matches if we have more than one item to search for
    
      * fix: range is exclusive, so use zero instead of one as the range-end (single-matches are valid)
    
      * fix: begin downwards iteration from the number-of-items-minus one; attempting a complete match has already been attempted in a previous code block

--- reciperadar/search/recipes.py
+++ reciperadar/search/recipes.py
@@ -268,8 +268,8 @@ class RecipeSearch(QueryRepository):
             yield query, sort_method, None
 
         positive_ingredients = sum([x.positive for x in ingredients])
-        if positive_ingredients:
-            for min_include_match in range(positive_ingredients, 1, -1):
+        if positive_ingredients > 1:
+            for min_include_match in range(positive_ingredients - 1, 0, -1):
                 for exact_match in [False]:
                     query, sort_method = self._render_query(
                         ingredients=ingredients,
