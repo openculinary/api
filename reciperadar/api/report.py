16714f008a4033618d5180ae2050e116eaa0cd4b
James Addison <james@reciperadar.com>
2025-01-28 14:45:18 +0000

    Add draft content-reporting API endpoint

---
+++ reciperadar/api/report.py
@@ -0,0 +1,23 @@
+from flask import abort, jsonify, request
+
+from reciperadar import app
+from reciperadar.models.recipes import Recipe
+from reciperadar.models.feedback import Feedback
+
+
+@app.route("/recipes/<recipe_id>/report", methods=["POST"])
+def recipe_report(recipe_id):
+    recipe = Recipe().get_by_id(recipe_id)
+    if not recipe:
+        return abort(404)
+
+    report_json = request.json
+    report_type = report_json["report_type"]
+    report_data = report_json[report_type]
+
+    Feedback.report(
+        report_type=report_type,
+        report_data=report_data,
+    )
+
+    return jsonify({"recipe_id": recipe_id})
