ec2c179a861f159bf8e28429df9ea1418159539a
James Addison <55152140+jayaddison@users.noreply.github.com>
2025-02-05 17:22:21 +0000

    Redirects: consider HTTP `GET` redirects as suspected bots (#128)

--- tests/api/test_redirect.py
+++ tests/api/test_redirect.py
@@ -3,11 +3,11 @@ from unittest.mock import call, patch
 from reciperadar.models.recipes import Recipe
 
 
-def _expected_redirect_call(recipe):
+def _expected_redirect_call(recipe, suspected_bot=False):
     return call(
         event_table="redirects",
         event_data={
-            "suspected_bot": False,
+            "suspected_bot": suspected_bot,
             "recipe_id": recipe.id,
             "domain": recipe.domain,
             "dst": recipe.dst,
@@ -28,14 +28,16 @@ def test_redirect_retrieval(get_recipe_by_id, store_event, client):
 
     assert response.status_code == 301
     assert response.location == recipe.dst
-    assert store_event.call_args == _expected_redirect_call(recipe)
+    assert store_event.call_args == _expected_redirect_call(recipe, suspected_bot=True)
 
 
+@patch("reciperadar.api.recipes.is_suspected_bot")
 @patch("reciperadar.api.recipes.store_event.delay")
 @patch.object(Recipe, "get_by_id")
-def test_redirect_ping(get_recipe_by_id, store_event, client):
+def test_redirect_ping(get_recipe_by_id, store_event, is_suspected_bot, client):
     recipe = Recipe(id="example_id", domain="example.test", dst="http://example.test")
     get_recipe_by_id.return_value = recipe
+    is_suspected_bot.return_value = False
 
     response = client.post(
         path="/redirect/recipe/example_id",
