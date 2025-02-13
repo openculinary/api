f84a3449b52061004786de27a7cff98f28db5a96
James Addison <james@reciperadar.com>
2025-02-13 12:01:58 +0000

    Problem reports: use Jinja templates to render HTML emails

--- reciperadar/models/feedback.py
+++ reciperadar/models/feedback.py
@@ -1,7 +1,13 @@
+from flask.templating import render_template
 from flask_mail import Message
 
 
 class Feedback:
+
+    @staticmethod
+    def _construct(subject, sender, recipients, html):
+        return Message(subject=subject, sender=sender, recipients=recipients, html=html)
+
     @staticmethod
     def distribute(issue, image):
         from reciperadar import app, mail
@@ -18,7 +24,7 @@ class Feedback:
                 html += "</tr>"
             html += f"</table><hr /><p>{description}</p></body></html>"
 
-            message = Message(
+            message = Feedback._construct(
                 subject=f"User feedback: {title}",
                 sender="contact@reciperadar.com",
                 recipients=["feedback@reciperadar.com"],
@@ -32,8 +38,14 @@ class Feedback:
         from reciperadar import app, mail
 
         with app.app_context():
-            msg = f"Please inspect {recipe_id!r} at {result_index} for {report_type}"
-            html = f"<html><body>{msg}</body></html>"
+            template = f"problem-report/{report_type}.html"
+            template_context = {
+                **report_data,
+                "recipe_id": recipe_id,
+                "report_type": report_type,
+                "result_index": result_index,
+            }
+            html = render_template(template, **template_context)
 
             # TODO: add per-report-type content inspection instructions
             #
@@ -54,7 +66,7 @@ class Feedback:
             #   b) confirm that the reported mistake exists in the recipe, and inspect for any other apparent problems.
             #   c) file a bugreport against the 'crawler' microservice with details of the expected and found results.
 
-            message = Message(
+            message = Feedback._construct(
                 subject=f"Content report: {report_type}: {recipe_id}",
                 sender="contact@reciperadar.com",
                 recipients=["content-reports@reciperadar.com"],
