2d0017ca47ee12894bbea697dbdaafcbb4a0f537
James Addison <james@reciperadar.com>
2025-02-27 15:20:18 +0000

    Problem reports: tests: fix and refactor test case

--- tests/models/test_feedback_model.py
+++ tests/models/test_feedback_model.py
@@ -21,13 +21,13 @@ def test_feedback(construct):
     )
     Feedback.register_report(recipe, report)
 
-    expected_msg = "Please inspect test_recipe_id for removal_request"
-    expected_html = f"<html><body>{expected_msg}</body></html>"
-
-    assert construct.call_args == call(
-        subject="Content report: removal_request: test_recipe_id",
-        sender="contact@reciperadar.com",
-        recipients=["content-reports@reciperadar.com"],
-        html=expected_html,
+    subject, sender, recipients, html = (
+        construct.call_args.kwargs.get(field)
+        for field in ("subject", "sender", "recipients", "html")
     )
-    assert message.send.called
+    assert subject == "Content report: removal_request: test_recipe_id"
+    assert sender == "contact@reciperadar.com"
+    assert recipients == ["content-reports@reciperadar.com"]
+    assert "recipe removal request" in html
+    assert "https://www.reciperadar.com/#action=view&id=test_recipe_id" in html
+    assert f'<a href="mailto:webmaster@example.test">' in html
